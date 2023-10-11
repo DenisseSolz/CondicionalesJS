cd98c520ad6dd9902856dc1a65cd6fed2617c0e3
Denisse Solorzano <dsolorzano3@ucol.mx>
2023-10-11 12:22:53 -0600

    Primera version

---
+++ index.js
@@ -0,0 +1,63 @@
+//Condicional If else
+
+/*let condicional = 2;
+document.write("Condiconal If-else<br>");
+
+if (condicional==4){
+    document.write("La condicional If se ejecuto")
+    }
+else if(condicional==2){
+document.write("La condicional If else se ejecuto")
+}
+else {
+        document.write("La condicional If NO se ejecuto")
+    }
+
+*/
+
+//Ejemplo de if al momento de pedir la hora a usuario
+
+/*
+let hora = prompt("Dime la hora");
+
+if (hora>=1 && hora <= 11){
+    document.write("Buenos dias!");
+}
+else if(hora>=12 && hora<=19){
+    document.write("Buenas tardes!");
+}
+else if(hora>=20 && hora<=24){
+    document.write("Buenas noches!");
+}
+else{
+    document.write("Hora no valida");
+}
+*/
+
+//Condicional Switch
+/*
+let suerte = prompt("Ingresa un numero del 1 al 6 para saber tu suerte!! :)")
+
+switch(suerte){
+    case "1":
+        document.write("Tu suerte es nula, tendras una mala semana :(");
+    break;
+    case "2":
+        document.write("TE ESPERA MUCHO DINERO!!!");
+    break;
+    case "3":
+        document.write("Nada bueno ni malo, todo sigue igual");
+    break;
+    case "4":
+        document.write("Te iras de viaje pronto!!!");
+    break;
+    case "5":
+        document.write("Tendras 10 en todos tus examenes :D");
+    break;
+    case "6":
+        document.write("Te romperas un pie ;(");
+    break;  
+    default:
+        document.write("Numero NO valido");           
+}
+*/
